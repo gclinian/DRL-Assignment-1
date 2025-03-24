4299dc39012161fbcbb14e77b7f4c9c16a40bc73
GCman <gclin.ian@gmail.com>
2025-03-24 08:04:24 +0800

    submit train script

--- train.py
+++ train.py
@@ -4,81 +4,95 @@ import pickle
 from simple_custom_taxi_env import SimpleTaxiEnv
 
 
-# state design
-def get_state(obs):
-    # decode obs
-    taxi_pos = (obs[0], obs[1])
+def update_passenger_picked(state, prev_action, passenger_picked):
+    taxi_pos = (state[0], state[1])
     stations = [(0,0) for i in range(4)]
-    stations[0] = (obs[2], obs[3])
-    stations[1] = (obs[4], obs[5])
-    stations[2] = (obs[6], obs[7])
-    stations[3] = (obs[8], obs[9])
-    obstacle_north = obs[10]
-    obstacle_south = obs[11]
-    obstacle_east = obs[12]
-    obstacle_west = obs[13]
-    passenger_look = obs[14]
-    destination_look = obs[15]
-
+    stations[0] = (state[2], state[3])
+    stations[1] = (state[4], state[5])
+    stations[2] = (state[6], state[7])
+    stations[3] = (state[8], state[9])
+    passenger_look = state[14]
     on_station = False
     for station in stations:
         if taxi_pos == station:
             on_station = True
             break
 
-    if passenger_look == 1 and on_station:
-        pass
+    if not passenger_picked and passenger_look == 1 and on_station and prev_action == 4:
+        return True
+        
+    if passenger_picked and prev_action == 5:
+        return False
+    
+    return passenger_picked
+
+def get_state_extend(state, passenger_picked):
+    return (state[0], state[1], state[10], state[11], state[12], state[13], state[14], state[15], passenger_picked)
 
 
-def get_action(state, q_table, epsilon, n_actions):
+def get_action(state_extend, q_table, epsilon, n_actions):
     """Choose an action using an epsilon-greedy policy."""
     # Initialize state in Q-table if unseen
-    if state not in q_table:
-        q_table[state] = np.zeros(n_actions)
+    if state_extend not in q_table:
+        q_table[state_extend] = np.zeros(n_actions)
     if random.uniform(0, 1) < epsilon:
         return random.randint(0, n_actions - 1)
     else:
-        return int(np.argmax(q_table[state]))
+        return int(np.argmax(q_table[state_extend]))
 
 def main():
     # Create the environment
-    env = SimpleTaxiEnv(grid_size=10, fuel_limit=500)
+    env = SimpleTaxiEnv(grid_size=10, fuel_limit=5000)
     n_actions = 6  # [Move Down, Move Up, Move Right, Move Left, PICKUP, DROPOFF]
     
     # Initialize Q-table as a dictionary: key=state (tuple), value=np.array of Q-values
     q_table = {}
     
     # Hyperparameters for Q-learning
-    alpha = 0.001         # Learning rate
+    alpha = 0.05         # Learning rate
     gamma = 0.995       # Discount factor
     epsilon = 1.0        # Initial exploration rate
     epsilon_min = 0.1    # Minimum exploration rate
-    epsilon_decay = 0.99997  # Decay factor for exploration rate
-    num_episodes = 70000  # Number of training episodes
-    max_steps = 500    # Maximum steps per episode
+    epsilon_decay = 0.9999924  # Decay factor for exploration rate
+    num_episodes = 300000  # Number of training episodes
+    max_steps = 5000    # Maximum steps per episode
 
     for episode in range(num_episodes):
         # size_limit = 5 + episode // 1000
         # grid_size = random.randint(5, size_limit)
         # env = SimpleTaxiEnv(grid_size=10, fuel_limit=5000)
         state, _ = env.reset()
+        passenger_picked = False
+        prev_passenger_picked = False
+        prev_action = None
+        state_extend = get_state_extend(state, passenger_picked)
         total_reward = 0
         done = False
         step = 0
-
+        
         while not done and step < max_steps:
             # Choose an action using epsilon-greedy
-            action = get_action(state, q_table, epsilon, n_actions)
+            action = get_action(state_extend, q_table, epsilon, n_actions)
             next_state, reward, done, _ = env.step(action)
-            total_reward += reward
-
-            # Ensure the next state is in the Q-table
-            if next_state not in q_table:
-                q_table[next_state] = np.zeros(n_actions)
             
+            prev_action = action
+            prev_passenger_picked = passenger_picked
+            passenger_picked = update_passenger_picked(state, prev_action, passenger_picked)
+            next_state_extend = get_state_extend(next_state, passenger_picked)
+            # Ensure the next state is in the Q-table
+            
+            if next_state_extend not in q_table:
+                q_table[next_state_extend] = np.zeros(n_actions)
+            
+            # reward shaping
+            if not prev_passenger_picked and passenger_picked:
+                reward += 5
+
+            total_reward += reward
             # Update Q-value using the Q-learning update rule
-            q_table[state][action] += alpha * (reward + gamma * np.max(q_table[next_state]) - q_table[state][action])
+            q_table[state_extend][action] += alpha * (reward + gamma * np.max(q_table[next_state_extend]) - q_table[state_extend][action])
             state = next_state
+            state_extend = next_state_extend
             step += 1
 
         # Decay epsilon
